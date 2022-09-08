d8cf64c7bebfaa6d4f68862ea1f3c91e031ff9e4
MohamedJamh <jam7.mohamed@gamil.com>
2022-09-08 09:10:51 +0100

    ajouter le challenge 13

---
+++ Semain1/langageC/Challenge13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int nombre;
+    printf("entrer un nombre: ");
+    scanf("%d",&nombre);
+    printf("la valeur en octal est : %o \n",nombre);
+    printf("la valeur en hexadecimal est : %X \n",nombre);
+
+}
